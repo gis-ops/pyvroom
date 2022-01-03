896f02b60fcb83ba5890a1301f411bc0ea455414
nilsnolde <nils.nolde@gmail.com>
2022-01-03 17:56:17 +0100

    xx

--- conan_install.bat
+++ conan_install.bat
@@ -1,5 +1,1 @@
-conan profile new default --detect
-conan profile update "settings.compiler=Visual Studio" default
-conan profile update "settings.compiler.version=16" default
-conan config set "storage.path=conan_data"
-conan install --build=openssl --install-folder conan_build .
+
